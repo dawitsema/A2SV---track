067d7ac1979146114e1144d43a300de1a74f879e
dawitsema <128223243+dawitsema@users.noreply.github.com>
2023-12-04 19:53:55 +0300

    Added solution for Minimum Time Visiting All Points

---
+++ Python_track/minimum-time-visiting-all-points.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        total = 0
+        for i in range(len(points)-1):
+            diff = max(abs(points[i][0]-points[i+1][0]), abs(points[i][1]-points[i+1][1]))
+            total += diff
+        
+        return total
+
+        
